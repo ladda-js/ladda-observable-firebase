5007c75ba0bdb0ab107cbf08ae8f884f63f84bc0
LFDM <1986gh@gmail.com>
2017-12-27 07:17:00 +0100

    chore(*) Produce a main and a module build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
 
 export default [
   {
     input: 'src/index.js',
-    output: {
-      file: 'dist/bundle.js',
-      format: 'cjs'
-    },
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ],
     plugins: [
       babel({
         exclude: ['node_modules/**']
